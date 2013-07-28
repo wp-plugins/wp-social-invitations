b07ffcf63ab17d5e208ad558ee6190083a933672
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-28 02:15:58 +0000

    tagging version 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/tags/1.3.1@747309 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/wsi.js
@@ -0,0 +1,17 @@
+/* Based on http://wordpress.org/extend/plugins/social-connect/ */
+
+(function($){ 
+	$(function(){
+		$(".service-filters a").click(function(){
+			popupurl = $("#wsi_base_url").val();
+			provider = $(this).attr("data-provider");
+
+			window.open(
+				MyAjax.url+"?action=wsi_authenticate&redirect_to"+encodeURIComponent(popupurl)+"&provider="+provider+ "&_ts=" + (new Date()).getTime(),
+				"hybridauth_social_sing_on", 
+				"location=1,status=0,scrollbars=1,width=900,height=530"
+			); 
+		});
+	});
+})(jQuery);
+
