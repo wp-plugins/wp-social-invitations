480d71ac9ba8b2f587847458af69af01a8f2b93f
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 18:06:32 +0000

    tagging version 1.4.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/tags/1.4.3@814515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/wsi.js
+++ assets/js/wsi.js
@@ -10,8 +10,17 @@
 		    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 		    var left = ((screen.width / 2) - (600 / 2)) + dualScreenLeft;
 		    var top = ((screen.height / 2) - (640 / 2)) + dualScreenTop;
+		    var widget_id = $(this).closest('.wsi-locker').attr('id');
+		    var wsi_locker = true;
+		    if( !widget_id ) 
+		    {
+		    	var widget_id = Date.now();
+		    	$(this).closest('.service-filter-content').attr('id',widget_id);
+				wsi_locker = false;
+		    	
+		    }	
 			window.open(
-				WsiMyAjax.admin_url+"?action=wsi_authenticate&redirect_to"+encodeURIComponent(popupurl)+"&provider="+provider+ "&_ts=" + (new Date()).getTime(),
+				WsiMyAjax.admin_url+"?action=wsi_authenticate&redirect_to="+encodeURIComponent(popupurl)+"&provider="+provider+ "&widget_id="+widget_id+"&wsi_locker="+wsi_locker+"&_ts=" + (new Date()).getTime(),
 				"hybridauth_social_sing_on", 
 				"directories=no,copyhistory=no,toolbar=0,location=0,menubar=0,status=0,scrollbars=1,width=600,height=640,top=" + top + ", left=" + left
 			); 
