5072ae280f4b12443312bae2ec831bed3d57fdf4
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-05 21:19:33 +0000

    deleting version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/trunk@870239 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/wsi.js
+++ assets/js/wsi.js
@@ -7,6 +7,7 @@
 			popupurl = $("#wsi_base_url").val();
 			provider = $(this).attr("data-provider");
 			var current_url = $('#wsi_base_url').val();
+			var obj_id 		= $('#wsi_obj_id').val();
 			var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
 		    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 		    var left = ((screen.width / 2) - (600 / 2)) + dualScreenLeft;
@@ -21,7 +22,7 @@
 		    	
 		    }	
 			window.open(
-				WsiMyAjax.admin_url+"?action=wsi_authenticate&redirect_to="+encodeURIComponent(popupurl)+"&provider="+provider+ "&widget_id="+widget_id+"&wsi_locker="+wsi_locker+"&current_url="+encodeURIComponent(current_url)+"&_ts=" + (new Date()).getTime(),
+				WsiMyAjax.admin_url+"?action=wsi_authenticate&redirect_to="+encodeURIComponent(popupurl)+"&provider="+provider+ "&widget_id="+widget_id+"&wsi_locker="+wsi_locker+"&current_url="+encodeURIComponent(current_url)+"&wsi_obj_id="+obj_id+"&_ts=" + (new Date()).getTime(),
 				"hybridauth_social_sing_on", 
 				"directories=no,copyhistory=no,toolbar=0,location=0,menubar=0,status=0,scrollbars=1,width=600,height=640,top=" + top + ", left=" + left
 			); 
