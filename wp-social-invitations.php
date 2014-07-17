5b92215979808f7d7fb93d8ebab4bf04974975e3
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-17 21:54:47 +0000

    changing version
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/tags/1.5.5@950668 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-social-invitations.php
+++ wp-social-invitations.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Social Invitations
 Plugin URI: http://wp.timersys.com/wordpress-social-invitations
 Description: Allow your visitors to invite friends of their social networks such as Twitter, Facebook, Linkedin, Google, Yahoo, Hotmail and more.
-Version: 1.5.4
+Version: 1.5.5
 Author: timersys
 Author URI: http://www.timersys.com
 License: MIT License
@@ -80,7 +80,7 @@ class WP_Social_Invitations extends WP_Plugin_Base_free
 		self::$PREFIX			=	'wsi';
 		$this->WPB_SLUG			=	'wp-social-invitations'; // Need to match plugin folder name
 		$this->WPB_PLUGIN_NAME	=	'Wordpress Social Invitatios';
-		$this->WPB_VERSION		=	'1.5.4';
+		$this->WPB_VERSION		=	'1.5.5';
 		$this->PLUGIN_FILE		=   plugin_basename(__FILE__);
 		$this->options_name		=   $this->WPB_PREFIX.'_settings';
 		$this->CLASSES_DIR		=	dirname( __FILE__ ) . '/classes';
