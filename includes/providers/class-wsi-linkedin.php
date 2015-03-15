4758637cca33870771f0851b33af939f1ee9d822
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-15 00:24:04 +0000

    tagging 2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/tags/2.0.3@1112865 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/providers/class-wsi-linkedin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class for Linkedin messages
+ *
+ * @link       http://wp.timersys.com/wordpress-social-invitations/
+ * @since      2.5
+ *
+ * @package    Wsi
+ * @subpackage Wsi/includes/providers
+ */
+
+class Wsi_Linkedin extends Wsi_Providers {
+
+	/*
+	 * Provider name
+	 */
+	public $name;
+	protected $hybridauth;
+	protected $provider;
+
+	/**
+	 * Class constructor
+	 */
+	function __construct( ) {
+		$this->name = 'linkedin';
+		$this->connect();
+	}
+
+}
