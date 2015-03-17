2d7eb6a50da723439df36ee685438a4182a57128
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-17 14:14:38 +0000

    Tagging 2.0.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/tags/2.0.4@1114646 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/senders/class-wsi-twitter.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Class for sending twitter messages
+ *
+ * @property  total_sent
+ *
+ * @link       http://wp.timersys.com/wordpress-social-invitations/
+ * @since      2.5
+ *
+ * @package    Wsi
+ * @subpackage Wsi/includes/senders
+ */
+
+class Wsi_Twitter_Sender extends Wsi_Senders {
+
+	protected $opts;
+
+	protected $data;
+	protected $limit;
+	protected $every;
+	protected $user_data;
+	protected $total_sent;
+	protected $hybridauth;
+	protected $adapter;
+	protected $sdata;
+
+	/**
+	 * Sender contructor
+	 *
+	 * @param $queue_data
+	 */
+	function __construct( $queue_data ) {
+		global $wsi_plugin;
+
+		add_filter('wsi/placeholders/accept_url', array( 'Wsi_Queue', 'shortenUrl'));
+		$this->opts     = $wsi_plugin->get_opts();
+		$this->limit	= 250;
+		$this->every	= 60 * 60 * 24; //one day
+		$this->setData($queue_data);
+		$this->connectSession();
+	}
+
+	/**
+	 * Send Twitter DM
+	 * @return mixed
+	 */
+	public function send() {
+		global $wpdb;
+
+		$delete_row = true;
+
+		$sent_on_batch = 0;
+
+
+		try {
+			$this->adapter->setUserStatus( $this->data->message );
+			$this->total_sent ++;
+			$sent_on_batch ++;
+			do_action( 'wsi/invitation_sent', $this->data->user_id, $this->data->wsi_obj_id );
+		} catch( Exception $e ) {
+			//if we reach here if that we wasn't able to post DM or status so just delete the record from queue to avoid getting stuck
+			$delete_row = true;
+		}
+
+		//save stats
+		Wsi_Logger::log_stat('twitter',$this->data->user_id, $sent_on_batch, $this->data->id,$this->data->display_name, $this->data->wsi_obj_id);
+
+		// we finish with this row, lets delete it
+		if( $delete_row ) $wpdb->query("DELETE FROM {$wpdb->prefix}wsi_queue WHERE id =  '".$this->data->id."'");
+
+		//Let's see if we have more in queue
+		$queue_data = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}wsi_queue WHERE provider = 'twitter' AND id >  '".$this->data->id."' AND display_name != '". $this->data->display_name ."' ORDER BY id ASC LIMIT 1");
+
+		//if we have more rows, proccess them
+		if( isset($queue_data->id) )
+		{
+			$this->setData($queue_data, 0);
+			$this->send();
+		}
+
+		return $this->total_sent;
+	}
+
+
+}
