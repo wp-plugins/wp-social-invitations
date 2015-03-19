99a8ee838f3b4be746dc0a4bcf4e28b036267c14
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-19 19:11:39 +0000

    tagging 2.0.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-social-invitations/trunk@1116251 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/providers/class-wsi-twitter.php
+++ includes/providers/class-wsi-twitter.php
@@ -27,4 +27,20 @@ class Wsi_Twitter extends Wsi_Providers {
 		$this->connect();
 	}
 
+	/**
+	 * Free version don't let you choose friends
+	 * Display collector for provider
+	 */
+	public function collector() {
+		global $wsi_plugin;
+		$opts = $wsi_plugin->get_opts();
+
+		$template = wsi_locate_template('popup/collector/share.php');
+		wsi_get_template('popup/collector.php', array(
+				'template' => $template,
+				'provider' => $this->name,
+			)
+		);
+		$this->provider->logout();
+	}
 }
